597554ca5d858205580e1c56196f5540b4806441
Jacob Werzinsky <f003frq@dartmouth.edu>
2020-03-03 04:19:59 -0500

    Update priority_queue.h

--- priority_queue.h
+++ priority_queue.h
@@ -1,50 +1,47 @@
 /* 
- * bag.h - header file for CS50 'bag' module
+ * priority_queue.h - header file for CS50 'priority_queue' module (modified from bag.h)
  * 
- * A ‘bag’ is a collection of indistinguishable items; it starts empty, 
- * grows as the caller adds one item at a time, and shrinks as the caller 
- * extracts one item at a time. It could be empty, or could contain hundreds
- * of items. Since items are indistinguishable, the module is free to return
- * any item from the bag. 
+ * A ‘priority_que’ is a collection of (item, integer) pairs. It starts 
+ * out empty and grows as (item, integer) pairs are added to it one at 
+ * a time. Each item is given a priority in the queue based off of the 
+ * passed integer, lower integers have lower priority. Items are 
+ * extracted, i.e. removed from the queue and returned to a caller, 
+ * one at a time and the item with the current lowest priority in the 
+ * queue is always extracted. 
  *
- * David Kotz, April 2016, 2017
- * Xia Zhou, July 2017, 2018, January 2019
- * Temi Prioleau, January 2020
+ * Jacob Werzinsky, CS50, Winter 2020
  */
 
-#ifndef __BAG_H
-#define __BAG_H
+#ifndef __PRIORITY_QUEUE_H
+#define __PRIORITY_QUEUE_H
 
 /**************** global types ****************/
-typedef struct bag bag_t;  // opaque to users of the module, users do not know and cannot access members of this struct
+typedef struct priority_queue priority_queue_t;
 
 /**************** functions ****************/
 
-/* Create a new (empty) bag; return NULL if error. */
-bag_t *bag_new(void);
+/* Create a new (empty) priority queue; return NULL if error. */
+priority_queue_t* priority_queue_new(void)
 
-/* Add new item to the bag; a NULL bag is ignored; a NULL item is ignored. */
-void bag_insert(bag_t *bag, void *item);
+/* Add new item to the queue with a priority; a NULL queue is ignored; a NULL item is ignored. */
+void priority_queue_insert(priority_queue_t *pq, void *item, int priority);
 
 /* Return any data item from the bag; return NULL if bag is NULL or empty. */
-void *bag_extract(bag_t *bag);
+void* priority_queue_extract(priority_queue_t *pq);
+
+/* */
+void* priority_queue_view(priority_queue_t *pq);
+
 
 /* Print the whole bag; provide the output file and func to print each item.
  * If fp==NULL; do nothing. If bag==NULL, print (null). 
  * If itemprint==NULL, print nothing for each item.
  */
-void bag_print(bag_t *bag, FILE *fp, 
-	       void (*itemprint)(FILE *fp, void *item));
-
-/* Iterate over the whole bag; call the given function on each item, 
- * passing both the item and an argument. Ignore if NULL bag or NULL itemfunc.
- */
-void bag_iterate(bag_t *bag, void *arg,
-		 void (*itemfunc)(void *arg, void *item) );
+void priority_queue_print(priority_queue_t *pq, FILE *fp, void (*itemprint)(FILE *fp, void *item));
 
 /* Delete the whole bag; ignore NULL bag.
  * Provide a function that will delete each item (may be NULL).
  */
-void bag_delete(bag_t *bag, void (*itemdelete)(void *item) );
+void priority_queue_delete(priority_queue_t *pq, void (*itemdelete)(void *item));
 
-#endif // __BAG_H
+#endif // __PRIORITY_QUEUE_H
